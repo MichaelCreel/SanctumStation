be53f545ca3dd92bd614bf9f397b61ce11e2d782
Michael Creel <michaelcreel815@gmail.com>
2025-12-02 21:42:59 -0700

    Added Comments Listing The Function Of Each Code File

---
+++ src/backend.py
@@ -0,0 +1,4 @@
+################################################################################
+# Python Backend for Sanctum Station
+# This file handles logic for the psuedo-desktop environment to be used.
+################################################################################
---
+++ src/main.js
@@ -0,0 +1,4 @@
+/*
+JavaScript Frontend for Sanctum Station
+This file handles interactive elements and transfer to the python backend for the psuedo-desktop environment to be used.
+*/
